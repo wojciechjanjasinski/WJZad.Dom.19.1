f0de7947e24d512e919d0c8d8d554fec3762ef1f
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-08-11 07:44:23 +0200

    I made corrections mainly in tests and some small in SumOfMultipleThreeAndFiveTogether.

--- src/main/java/SumOfMultipleThreeAndFiveTogether.java
+++ src/main/java/SumOfMultipleThreeAndFiveTogether.java
@@ -3,9 +3,9 @@ import java.util.stream.IntStream;
 
 public class SumOfMultipleThreeAndFiveTogether {
 
-    public static int generateSumOfMultipleThreeAndFive(int limitFromUser) {
-        return IntStream.range(0, limitFromUser)
-                .filter(number -> number % 3 ==0 || number % 5 == 0)
+    public static int generateSumOfMultipleThreeAndFive(int limit) {
+        return IntStream.range(0, limit)
+                .filter(number -> number % 3 == 0 || number % 5 == 0)
                 .sum();
     }
 }
--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -8,72 +8,58 @@ public class TestMain {
     public void shouldReturnTwentyThree() {
         //given
         final int givenForTestPurpose = 10;
-
-
+        //when
+        int sum = SumOfMultipleThreeAndFiveTogether.generateSumOfMultipleThreeAndFive(givenForTestPurpose);
         //then
         final int expectedValue = 23;
-        int sum = SumOfMultipleThreeAndFiveTogether.generateSumOfMultipleThreeAndFive(givenForTestPurpose);
-
-        //when
         assertThat(sum)
                 .isEqualTo(expectedValue);
     }
+
     @Test
     public void shouldReturnThirtyThree() {
         //given
         final int givenForTestPurpose = 11;
-
-
+        //when
+        int sum = SumOfMultipleThreeAndFiveTogether.generateSumOfMultipleThreeAndFive(givenForTestPurpose);
         //then
         final int expectedValue = 33;
-        int sum = SumOfMultipleThreeAndFiveTogether.generateSumOfMultipleThreeAndFive(givenForTestPurpose);
-
-        //when
         assertThat(sum)
                 .isEqualTo(expectedValue);
     }
+
     @Test
     public void shouldReturnThirtyThreeByGivenTwelve() {
         //given
         final int givenForTestPurpose = 12;
-
-
+        //when
+        int sum = SumOfMultipleThreeAndFiveTogether.generateSumOfMultipleThreeAndFive(givenForTestPurpose);
         //then
         final int expectedValue = 33;
-        int sum = SumOfMultipleThreeAndFiveTogether.generateSumOfMultipleThreeAndFive(givenForTestPurpose);
-
-        //when
-
         assertThat(sum)
                 .isEqualTo(expectedValue);
     }
+
     @Test
     public void shouldReturnFortyFive() {
         //given
         final int givenForTestPurpose = 13;
-
-
+        //when
+        int sum = SumOfMultipleThreeAndFiveTogether.generateSumOfMultipleThreeAndFive(givenForTestPurpose);
         //then
         final int expectedValue = 45;
-        int sum = SumOfMultipleThreeAndFiveTogether.generateSumOfMultipleThreeAndFive(givenForTestPurpose);
-
-        //when
-
         assertThat(sum)
                 .isEqualTo(expectedValue);
     }
+
     @Test
     public void shouldReturnSixty() {
         //given
         final int givenForTestPurpose = 16;
-
-
+        //when
+        int sum = SumOfMultipleThreeAndFiveTogether.generateSumOfMultipleThreeAndFive(givenForTestPurpose);
         //then
         final int expectedValue = 60;
-        int sum = SumOfMultipleThreeAndFiveTogether.generateSumOfMultipleThreeAndFive(givenForTestPurpose);
-
-        //when
-
         assertThat(sum)
                 .isEqualTo(expectedValue);
     }
